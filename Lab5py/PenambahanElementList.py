4feac9ac0467c42066b7efaaa8819f7fe92c600f
alipurnama <alipurnama69@gmail.com>
2022-11-16 23:38:22 +0700

    Perubahan element pada list

---
+++ Lab5py/PenambahanElementList.py
@@ -0,0 +1,12 @@
+a = [10]
+b = [20,30]
+
+#Penambahan 1 element di akhir menggunakan append
+a.append(40)
+
+#Penambahan beberapa item di akhir menggunakan extend
+b.extend([40, 50, 60])
+
+#Hasil Perubahan
+print(a)
+print(b)
---
+++ Lab5py/PenggabunganList.py
@@ -0,0 +1,13 @@
+a = [10]
+b = [20,30]
+
+#Menggabungkan 2 list ke list baru
+c = a + b
+
+#Menggabungkan list yang sudah ada
+a.extend(b)
+
+
+#Hasil Perubahannya
+print(c)
+print(a)
